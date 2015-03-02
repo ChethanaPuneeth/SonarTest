f6e4bfeb29275f470109187063210f70ceb02e1b
swatiykm <SY00354662@techmahindra.com>
2015-03-02 09:47:39 +0530

    Initial commit

---
+++ SonarTest/src/com/sonar/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.sonar.test;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Check sonar test");
+	}
+
+}
